50439147582489b26ed7167f9d757e288c35d17a
Majid Parvin <majid.parvin@seb.se>
2021-04-30 18:32:25 +0200

    clean up

--- OrderStore.cs
+++ OrderStore.cs
@@ -5,18 +5,8 @@ using System.Linq;
 
 namespace TradingEngine
 {
-    public class OrderStore : ObservableCollection<Order>
+    public class OrderStore : List<Order>
     {
-        public OrderStore()
-        {
-           
-        }
-
-        private void OrderStore_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
         public List<Order> Bids => this.Where(_ => _.IsBid).ToList();
         public IEnumerable<Order> Asks => this.Where(_ => !_.IsBid);
         public decimal? BestBid => Bids.DefaultIfEmpty(null).Max(bid => bid?.Price);
